2b5bc2ea58c6aaa53f96e6fb91c52b6e3c920b5e
rzx <82148134+rzxx@users.noreply.github.com>
2023-03-16 14:27:35 +0300

    updated to classes

--- main.py
+++ main.py
@@ -1,55 +1,73 @@
 import random
  
-hp1 = 100
-hp2 = 100
-at1 = 10
-at2 = 100
+class Enemy():
+    def __init__(self, health_, attack_, heal_):
+        self.health = health_
+        self.attack = attack_
+        self.heal = heal_
+ 
+class Player():
+    def __init__(self, health_, attack_, heal_):
+        self.health = health_
+        self.attack = attack_
+        self.heal = heal_
  
 # 1 - at
 # 2 - he
 # 3 - def
 print("Dark Souls 4.\n1 - атака\n2 - лечение\n3 - защита")
+def Fight(player, enemy):
+    while True:
+        move = int(input())
+        print("\n")
+        try:
+            enMove = random.randint(1, 3)
+        except:
+            print("неверный ввод!")
+            continue
+        if move != 1 and move != 2 and move != 3:
+            print("неверный ввод!")
+            continue
+        if move == 1:
+            print("//ты атаковал")
+            if enMove != 3:
+                enemy.health -= player.attack
+                print("попадание", player.attack)
+            else:
+                print("не пробил")
+        elif move == 2:
+            player.health += player.heal
+            print("//ты вылечился")
+            print("увеличено: ", player.heal)
+        else:
+            print("//защита")
+    
+        if enMove == 1:
+            print("//враг атаковал")
+            if move != 3:
+                print("попадание", enemy.attack)
+                player.health -= enemy.attack
+            else:
+                print("не пробил")
+        elif enMove == 2:
+            print("//враг лечится")
+            print("увеличено: ", enemy.heal)
+            enemy.health += enemy.heal
+        else:
+            print("//враг защищается")
+    
+        print("\n//твои хп: ", player.health, "хп врага: ", enemy.health)
+        print("\n")
+        if player.health <= 0:
+            print("поражение")
+            break
+        elif enemy.health <= 0:
+            print("победа")
+            break
+ 
+ 
+ 
 while True:
-    move = int(input())
-    print("\n")
-    enMove = random.randint(1, 3)
-    if move == 1:
-        print("//ты атаковал")
-        if enMove != 3:
-            hp2 -= at1
-            print("попадание", at1)
-        else:
-            print("не пробил")
-    elif move == 2:
-        hp1 += 10
-        print("//ты вылечился")
-        print("увеличено: ", 10)
-    else:
-        print("//защита")
- 
-    if enMove == 1:
-        print("//враг атаковал")
-        if move != 3:
-            print("попадание", at2)
-            hp1 -= at2
-        else:
-            print("не пробил")
-    elif enMove == 2:
-        print("//враг лечится")
-        print("увеличено: ", 2)
-        hp2 += 2
-    else:
-        print("//враг защищается")
- 
-    print("\n//твои хп: ", hp1, "хп врага: ", hp2)
-    print("\n")
-    if hp1 <= 0:
-        print("поражение")
-        print("\nновая игра\n\n")
-        hp1 = 100
-        hp2 = 100
-        continue
-    elif hp2 <= 0:
-        print("победа")
-        break
- 
+    player = Player(100, 10, 10)
+    enemy1 = Enemy(100, 100, 2)
+    Fight(player, enemy1)
